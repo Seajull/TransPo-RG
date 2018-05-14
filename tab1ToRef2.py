e3ff0bed2012584168f2b744f866fac6aecc27b1
Bellot Clément <clement.bellot@cirad.fr>
2018-05-14 15:35:10 +0200

    samToTab vcf ok, dir résultat

--- tab1ToRef2.py
+++ tab1ToRef2.py
@@ -35,34 +35,48 @@ if args.tabinput == None:
 if args.fasta2 == None:
     sys.exit("ERREUR : L'option --fasta2 (-f2) est manquant")
 res1 = re.search("/?(\w+)\.",args.fasta1)
-if args.fasta2 :
-    res2 = re.search("/?(\w+)\.",args.fasta2)
-    if args.out == None:
-        args.out = "data/aln_"+res1.group(1)+"_"+res2.group(1)+".sam"
-    else :
-        args.out = "data/"+args.out
+res2 = re.search("/?(\w+)\.",args.fasta2)
+if args.out == None:
+    args.out = "./résultat/aln_"+res1.group(1)+"_"+res2.group(1)+".sam"
+else :
+    args.out = "./résultat/"+args.out
 
 
 lenChr = tempfile.NamedTemporaryFile()
 
+try :
+    os.mkdir("./résultat")
+    if args.verbose :
+        print("\n ----- Création du dossier résultat -----")
+except :
+    pass
+
 if not args.tempf : 
+   
     tabOut=tempfile.NamedTemporaryFile()
     tabO=tabOut.name
+    tabOPut=tempfile.NamedTemporaryFile()
+    tabOP=tabOPut.name
     selectS = tempfile.NamedTemporaryFile()
     selectedSeq = selectS.name
 else :
+    
+    tabOut=tempfile.NamedTemporaryFile()
+    tabO=tabOut.name
     fasta1Out=(args.fasta1).split(".")
     selectedSeq=fasta1Out[0]+"_selected."+fasta1Out[1]
     tab=(args.tabinput).split(".")
-    tabO = (tab[0]+"_out."+tab[1])
+    tabOP="./résultat/"+(tab[0].split("/")[-1]+"_out."+tab[1])
+
 
 fileTab=BedTool(args.tabinput)
+
 
 if args.typeF != None and fileTab.file_type != "gff" :
     print("")
     warnings.warn("L'option --type (-t) est ignorée car le fichier tabulé n'est pas au format GFF.",Warning)
 
-# Méthode permettant de récupérer l'ID des chromosomes et leurs tailles
+# Méthode permettant de récupérer l'ID des chromosomes et leurs tailles à l'aide de biopython
 
 def parseFa() :
     with open(lenChr.name,"w") as lenC : 
@@ -74,19 +88,37 @@ parseFa()
 
 # Test si index des chromosomes existant sinon on le réalise
 
+def cutGff() :
+    splitType = args.typeF.split(",")
+    typeFclean=[]
+    
+    for t in splitType :
+        t=t.lower()
+        if t not in typeFclean :
+            typeFclean.append(t)
+    if args.verbose :
+        print("\n ----- Récupération des features de type : \""+",".join(typeFclean)+"\" dans le GFF. -----")
+    typeFclean="|".join(typeFclean)
+    with open(tabO,"w") as out :
+        call(["awk","tolower($3) ~ /"+typeFclean+"/",args.tabinput],stdout=out)
+    return 
+
 def getFlank() :
+    fileTab2=BedTool(args.tabinput)
+    if args.typeF != None and fileTab2.file_type == "gff" :
+        fileTab2=BedTool(tabO)
     if args.verbose and args.tempf:
-        print("\n ----- Création du fichier " +tabO +". ----- ")
+        print("\n ----- Création du fichier " +tabOP +". ----- ")
         
-    if (fileTab.file_type != "vcf"):
-        fileTab.slop(b=args.flank, g=lenChr.name ,output=tabO)
+    if (fileTab2.file_type != "vcf"):
+        fileTab2.slop(b=args.flank, g=lenChr.name ,output=tabOP)
         # Pour les fichiers VCF, on doit rajouter une colonne. On recrée donc un objet BedTools à partir
         # du fichier VCF original.
         
     else :
         with open(lenChr.name,"r") as lenC :
             res=""
-            for feature in fileTab :
+            for feature in fileTab2 :
                 for line in lenC :
                     lenghtC=re.search(feature.chrom+"\t(\d+)",line)
                     if lenghtC :
@@ -98,31 +130,16 @@ def getFlank() :
                 if feature.start-args.flank < 0 :
                     start=0
                 else :
-                    start=feature.start-args.flank
+                    start=feature.start-args.flank-1
                 res += feature.chrom +" "+str(start)+" "+ str(stop)+"\n"
-        BedTool(res, from_string=True).saveas(tabO)
+        BedTool(res, from_string=True).saveas(tabOP)
     return 
-    # Fichier [nom fichier tabulé]_out.[ext] (nom stocké dans tabOut) en sortie
-    
-def cutGff() :
-    splitType = args.typeF.split(",")
-    typeFclean=[]
-    for t in splitType :
-        t=t.lower()
-        if t not in typeFclean :
-            typeFclean.append(t)
-    if args.verbose :
-        print("\n ----- Récupération des features de type : \""+",".join(typeFclean)+"\" dans le GFF. -----")
-    typeFclean="|".join(typeFclean)
-    with open(tabO,"w") as out :
-        call(["awk","tolower($3) ~ /"+typeFclean+"/",args.tabinput],stdout=out)
-    return 
-    
+    # Fichier [nom fichier tabulé]_out.[ext] (nom stocké dans tabOut) en sortie  
 
 def getFasta() :
     if args.verbose :
         print("\n ----- Récupération des séquences flanquantes dans le fichier "+args.fasta1+". -----")
-    BedTool(tabO).sequence(fi=args.fasta1).save_seqs(selectedSeq)
+    BedTool(tabOP).sequence(fi=args.fasta1).save_seqs(selectedSeq)
     # Sauvegarde dans le fichier désigné par la variable selectedSeq les séquences fasta correspondantes au informations contenue dans le fichier tabulé
     return 
 
@@ -160,17 +177,17 @@ def samToTab() :
                     for f in samf :
                         res=re.search(":(\d+)-\d+",f[0])
                         if res :
-                            if int(res.group(1)) == int(line[1])-50 :
-                                pos=int(f[3])+49
+                            if int(res.group(1)) == int(line[1])-args.flank-1 :
+                                pos=int(f[3])+args.flank
                                 #print(pos)
                                 break
-                    if fileTab.file_type == "vcf" and f[5]=="100M":
+                    if fileTab.file_type == "vcf" and f[5]==str(args.flank*2+1)+"M": # Match parfait uniquement pour un SNP
                         tabou+=f[2]+" "+ str(pos) +" "+ " ".join(line[2:11])
                     elif fileTab.file_type == "bed" :
                         tabou+=f[2]+" "+ str(pos) +" "+ str(pos) +" "+ " ".join(line[2:3])
                     elif fileTab.file_type == "gff" :
-                        pass            
-            BedTool(tabou,from_string=True).saveas("out.vcf")
+                        pass
+            BedTool(tabou,from_string=True).saveas("./résultat/"+res2.group(1)+"_out.vcf")
     return
 
 def getPosCds(tab,flank=0) :
@@ -205,10 +222,11 @@ def getPosCds(tab,flank=0) :
 #dicoPos2=getPosCds(tabout,args.flank)
 
 
-getFlank()
+
 
 if args.typeF != None and fileTab.file_type == "gff" :
     cutGff()
+getFlank()
 index()
 align()
 if fileTab.file_type == "vcf":
--- test_dir/extractSeq.py
+++ test_dir/extractSeq.py
@@ -4,11 +4,10 @@ import sys, re, os
 fasta1=SeqIO.parse(sys.argv[1],"fasta")
 fasta2=SeqIO.parse(sys.argv[2],"fasta")
 for i in fasta1 :
-    #print(i.seq[0:100])
-    print(i.seq[42343:42444])
-    print(i.seq[42392:42395])
-   # print(i.seq[19211:19311])
+    print(i.seq[19047])
+    print(i.seq[32667])
+    print(i.seq[125489])
 for i in fasta2 :
-    #print(i.seq[84:87])
-    print(i.seq[42135:42236])
-    print(i.seq[42184:42187])
+    print(i.seq[19260])
+    print(i.seq[37306])
+    print(i.seq[78150])
