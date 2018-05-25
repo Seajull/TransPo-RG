176c8834a434c48a6ecff0510597943d6536096b
Bellot Clément <clement.bellot@cirad.fr>
2018-05-25 09:06:51 +0200

    commit d'hier

--- test_dir/extractSeq.py
+++ test_dir/extractSeq.py
@@ -2,12 +2,12 @@ from Bio import SeqIO
 import sys, re, os
 
 fasta1=SeqIO.parse(sys.argv[1],"fasta")
-fasta2=SeqIO.parse(sys.argv[2],"fasta")
+#fasta2=SeqIO.parse(sys.argv[2],"fasta")
 for i in fasta1 :
-    print(i.seq[19047])
-    print(i.seq[32667])
-    print(i.seq[125489])
-for i in fasta2 :
-    print(i.seq[19260])
-    print(i.seq[37306])
-    print(i.seq[78150])
+    print(i.seq[135])
+    print(i.seq[155])
+    print(i.seq[125577])
+    #for i in fasta2 :
+        #print(i.seq[19260])    
+        #print(i.seq[37306])    
+        #print(i.seq[78150])
