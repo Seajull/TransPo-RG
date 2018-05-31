8a8f3a80fedca889ec7c7ac5b2770f71b0e9cfdd
Bellot Clément <clement.bellot@cirad.fr>
2018-05-31 11:00:43 +0200

    version.py now check git log instead of git tag

--- version.py
+++ version.py
@@ -1,39 +1,25 @@
 from subprocess import check_output
-import sys, os, datetime
+import sys, os, datetime, re
 
 def updateTag() :
-    de = datetime.datetime.now()
-    date=de.strftime("%d %b %Y")
-    ver=".version"
     if not os.path.isdir(".git"):
         sys.exit("This does not appear to be a Git repository.")
-    if not os.path.isfile(".version") :
-        out = check_output(["git", "tag"]).decode("utf-8")
-        vers=""
-        with open(".version","a") as verOut :
-            print("File '.version' created.")
-            for i in out :
-                if i != "v" and i != "\n" :
-                    vers+=i
-                if i == "\n" :
-                    verOut.write(vers+" - "+date+"\n")
-                    vers=""
-    else :
-        out = check_output(["git", "describe","--long", "--tags"]).decode("utf-8")
-        vers=(out.split("-")[0][1:]+"\n")
-        with open(".version","a+") as verOut :
-            verOut.seek(0)
-            f=verOut.readlines()
-            if f[-1].split(" - ")[0]!= vers[:-1] :
-                verOut.write(vers+" - "+date+"\n")
-            else :
-                print("Version file already up-to-date.")
+    out = check_output(["git", "log", "--tags", "--simplify-by-decoration", "--pretty='format=:%aD %d'"]).decode("utf-8")
+    vers=""
+    outs=out.split("\n")
+    with open(".version","w") as verOut :
+        print("File '.version' created.")
+        for i in outs :
+            date=re.search(":\w+,\s(\d+\s\w+\s\d+)\s",i)
+            tag=re.search("tag:\s\w(\d+\.\d+\.\d+)",i)
+            if date and tag :
+                verOut.write(tag.group(1) + " - " +date.group(1)+"\n")
 
 def getVersion() :
     with open(".version","r") as verF :
         f=verF.readlines()
-        print("\n"+"Version : "+f[-1].split(" - ")[0])
-        print("Last update : "+f[-1].split(" - ")[1])
+        print("\n"+"Version : "+f[0].split(" - ")[0])
+        print("Last update : "+f[0].split(" - ")[1])
     return
 
 if __name__ == "__main__" :
